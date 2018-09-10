70ae7ce85dd99b456d0472e7d5bd1267821547fe
joshua-david <joshrobson4@gmail.com>
2018-09-10 12:35:36 +0100

    bespoke twitter shares

--- src/js/lib/share.js
+++ src/js/lib/share.js
@@ -6,9 +6,25 @@ export default function share(title, shareURL) {
     return function (network, extra='') {
         var twitterMessage = `${extra}${title}`;
         var shareWindow;
+        var el = document.querySelector('body');
+        var film = el.getAttribute('data-this-film');
+
+
 
         if (network === 'twitter') {
-            shareWindow = twitterBaseUrl + encodeURIComponent(twitterMessage + ' #BeyondtheBlade ') + shareURL;
+            if (film === 'father') {
+                shareWindow = twitterBaseUrl + encodeURIComponent(twitterMessage + " #BeyondtheBlade 'Wrong place, wrong time – that's it' gu.com/blade-father ");
+            }
+            if (film === 'mother') {
+                shareWindow = twitterBaseUrl + encodeURIComponent(twitterMessage + " #BeyondtheBlade 'When they found him guilty, the found me guilty too' gu.com/blade-mother ");
+            }
+            if (film === 'boy') {
+                shareWindow = twitterBaseUrl + encodeURIComponent(twitterMessage + " #BeyondtheBlade 'I didn’t mean for him to die' gu.com/blade-boy ");
+            }
+            if (film === 'doctor') {
+                shareWindow = twitterBaseUrl + encodeURIComponent(twitterMessage + " #BeyondtheBlade 'You can’t just keep patching them up and sending them back out there without any support' gu.com/blade-doctor ");
+            }
+
         } else if (network === 'facebook') {
             shareWindow = facebookBaseUrl + shareURL;
         } else if (network === 'email') {
